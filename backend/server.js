5f8340dcc04ff148cbfe00bd32bd1d34b0749c27
Manasi2k25 <manasi.official2024@gmail.com>
2025-10-14 18:45:39 +0530

    Added comments wherever required.

--- backend/server.js
+++ backend/server.js
@@ -176,5 +176,6 @@ app.delete('/tasks/:id', async (req, res) => {
 
 // ============ SERVER ============
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.listen(5000, '0.0.0.0', () => console.log("✅ Server running on http://0.0.0.0:5000"));
 
